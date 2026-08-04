1b2f2aa7a6fdd2e88020f2b18e1a430b015cc2a0
Bhavana1612 <bhavanakanasani1612@gmail.com>
2026-08-04 08:43:38 +0530

    Configure database for production deployment

--- app/database.py
+++ app/database.py
@@ -1,9 +1,27 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/ingestion_db"
 
-engine = create_engine(DATABASE_URL)
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "postgresql://postgres:postgres@localhost:5432/ingestion_db"
+)
+
+# Render gives postgres:// but SQLAlchemy needs postgresql://
+if DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace(
+        "postgres://",
+        "postgresql://",
+        1
+    )
+
+
+engine = create_engine(
+    DATABASE_URL
+)
+
 
 SessionLocal = sessionmaker(
     autocommit=False,
@@ -11,12 +29,15 @@ SessionLocal = sessionmaker(
     bind=engine
 )
 
+
 Base = declarative_base()
 
 
 def get_db():
     db = SessionLocal()
+
     try:
         yield db
+
     finally:
         db.close()
