d76a47f024d996cb394288de267023dafcf324c1
Bhavana1612 <bhavanakanasani1612@gmail.com>
2026-08-04 07:13:02 +0530

    Support multiple external API ingestion

--- app/routes/ingest.py
+++ app/routes/ingest.py
@@ -1,7 +1,9 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from app.database import get_db
 from app.models import IngestedData
+from app.schemas import EndpointRequest
+import requests
 
 
 router = APIRouter(
@@ -11,21 +13,38 @@ router = APIRouter(
 
 
 @router.post("/")
-def ingest_data(data: dict, db: Session = Depends(get_db)):
+def ingest_data(request: EndpointRequest, db: Session = Depends(get_db)):
 
-    record = IngestedData(
-        name=data.get("name"),
-        data=data
-    )
+    saved_records = []
 
-    db.add(record)
-    db.commit()
-    db.refresh(record)
+    for endpoint in request.endpoints:
+
+        try:
+            response = requests.get(endpoint)
+            response.raise_for_status()
+
+            api_data = response.json()
+
+        except Exception as e:
+            raise HTTPException(
+                status_code=400,
+                detail=f"Failed to fetch {endpoint}: {str(e)}"
+            )
+
+        record = IngestedData(
+            name="External API",
+            data=api_data
+        )
+
+        db.add(record)
+        db.commit()
+        db.refresh(record)
+
+        saved_records.append(record.id)
 
     return {
-        "message": "Data ingestion successful",
-        "id": record.id,
-        "received_data": data
+        "message": "All API data ingested successfully",
+        "saved_records": saved_records
     }
 
 
